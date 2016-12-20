738e15adb4cde58dff5b6af5295d1a6f793eab1e
Christopher Stott <christopherstott@gmail.com>
2016-12-20 14:16:56 -0800

    Add some NSLogs

--- BBTestAppTests/Calculator.swift
+++ BBTestAppTests/Calculator.swift
@@ -10,14 +10,17 @@ import Foundation
 
 class Calculator {
     class func Add(a: Int, b: Int) -> Int {
+        NSLog("NSLog : Calculator.Add")
         return a + b
     }
     
     class func Subtract(a: Int, b: Int) -> Int {
+        NSLog("NSLog : Calculator.Subtract")
         return a - b
     }
     
     class func Multiply(a: Int, b: Int) -> Int {
+        NSLog("NSLog : Calculator.Multiply")
         if (a == 7) {
             return 7 * a * b
         }
@@ -26,10 +29,12 @@ class Calculator {
     }
     
     class func Add2(a: Int, b: Int) -> Int {
+        NSLog("NSLog : Calculator.Add2")
         return Int(ObjCCalculator.add(a, to: b))
     }
     
     class func Subtract2(a: Int, b: Int) -> Int {
+        NSLog("NSLog : Calculator.Subtract2")
         return Int(ObjCCalculator.subtract(a, from: b))
     }
 }
