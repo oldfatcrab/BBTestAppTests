b26bf348470efa94da92fb86454647ad0b368521
Golson Xie <golson@buddybuild.com>
2017-06-30 15:57:04 -0700

    fail

--- BBTestAppTestsTests/CalculatorTests.swift
+++ BBTestAppTestsTests/CalculatorTests.swift
@@ -30,7 +30,7 @@ class CalculatorTests: XCTestCase {
     }
     
     func testMultiply() {
-        XCTAssertEqual(Calculator.Multiply(a: 3, b: 5), 24);
+        XCTAssertEqual(Calculator.Multiply(a: 3, b: 5), 23);
     }
     
 }
